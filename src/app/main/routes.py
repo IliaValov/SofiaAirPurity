75e57165189e02cd13ac29cdc54dba3dc70439fb
Pavel Sarlov <sarlovpavel@gmail.com>
2021-11-24 17:49:22 +0200

    switched to plotly

--- src/app/main/routes.py
+++ src/app/main/routes.py
@@ -1,8 +1,22 @@
-from flask import current_app as app, g, session, render_template, redirect, url_for, Blueprint
-import os
+from flask import current_app as app, g, session, render_template, redirect, url_for, Blueprint, jsonify
+import os, random
+from datetime import timedelta, date
 
 main = Blueprint("main", __name__)
 
 @main.route("/")
 def index():
     return render_template("index.html")
+
+# Just testing requests
+@main.route("/data")
+def data():
+    def daterange(date1, date2):
+        for n in range(int ((date2 - date1).days)+1):
+            yield date1 + timedelta(n)
+    labels = [dt.strftime("%Y-%m-%d") for dt in daterange(date(2013,3,9), date(2016,3,5))][1:50]
+    values = [random.randrange(0,100) for x in labels]
+    return jsonify({
+        "labels": labels,
+        "values": values
+        })
--- src/app/static/js/chart.js
+++ src/app/static/js/chart.js
@@ -1,113 +1,107 @@
-// <block:actions:2>
-// const actions = [
-//   {
-//     name: 'Randomize',
-//     handler(chart) {
-//       chart.data.datasets.forEach(dataset => {
-//         dataset.data = Utils.bubbles({count: chart.data.labels.length, rmin: 1, rmax: 1, min: 0, max: 100});
-//       });
-//       chart.update();
-//     }
-//   },
-//   {
-//     name: 'Add Dataset',
-//     handler(chart) {
-//       const data = chart.data;
-//       const dsColor = Utils.namedColor(chart.data.datasets.length);
-//       const newDataset = {
-//         label: 'Dataset ' + (data.datasets.length + 1),
-//         backgroundColor: Utils.transparentize(dsColor, 0.5),
-//         borderColor: dsColor,
-//         data: Utils.bubbles({count: data.labels.length, rmin: 1, rmax: 1, min: 0, max: 100}),
-//       };
-//       chart.data.datasets.push(newDataset);
-//       chart.update();
-//     }
-//   },
-//   {
-//     name: 'Add Data',
-//     handler(chart) {
-//       const data = chart.data;
-//       if (data.datasets.length > 0) {
-
-//         for (let index = 0; index < data.datasets.length; ++index) {
-//           data.datasets[index].data.push(Utils.bubbles({count: 1, rmin: 1, rmax: 1, min: 0, max: 100})[0]);
-//         }
-
-//         chart.update();
-//       }
-//     }
-//   },
-//   {
-//     name: 'Remove Dataset',
-//     handler(chart) {
-//       chart.data.datasets.pop();
-//       chart.update();
-//     }
-//   },
-//   {
-//     name: 'Remove Data',
-//     handler(chart) {
-//       chart.data.labels.splice(-1, 1); // remove the label first
-
-//       chart.data.datasets.forEach(dataset => {
-//         dataset.data.pop();
-//       });
-
-//       chart.update();
-//     }
-//   }
-// ];
-// </block:actions>
-
 window.onload = function() {
-    const datasetLabel = "Test";
-    const labels = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul'];
-    const values = [20, 21, 10, 30, 50, 55, 70];
-    const data = {
-        labels: labels,
-        datasets: [{
-            label: datasetLabel,
-            data: values,
-            borderColor: 'rgb(255,0,2)',
-            backgroundColor: 'rgba(255,0,0,0.5)',
-        }, ]
-    };
+    const dataEndpoint = "/data";
+    var chartType = $("#chartType").get(0);
+    var station = $("#station").get(0);
+    var airType = $("#airType").get(0);
+    var allData = null;
 
-    const config = {
-        type: 'line',
-        data: data,
-        options: {
-            responsive: true,
-            plugins: {
-                legend: {
-                    position: 'right',
-                },
-                title: {
-                    display: true,
-                    text: 'Test Chart',
-                    font: {
-                        size: '50px',
-                        weight: 'bold',
-                    },
-                },
-                scales: {
-                    xAxes: [{
-                        scaleLabel: {
-                            display: true,
-                            labelString: 'test',
-                        },
-                    }]
+    var myChart = $("#myChart").get(0);
+    var chartData = null;
+    var chartLayout = null;
+    var chartConfig = null;
+
+    function errMsg() {
+        var errMsg = $("<div id='errMsg'>ЛИПСВАТ ДАННИ ИЛИ СА НЕВАЛИДНИ &#x2639;</div>");
+        $(".chart-container").get(0).appendChild(errMsg.get(0));
+    }
+
+    function updateChart() {
+        const type = chartType.value;
+        var mode = "lines+markers";
+        const labels = allData ? allData["labels"] : [];
+        const values = allData ? allData["values"] : [];
+        const title = station.options[station.selectedIndex].text;
+        const datasetLabel = airType.options[airType.selectedIndex].text;
+
+        if (type == "scatter") {
+            mode = "markers";
+        }
+
+        chartData = [
+            {
+                name: datasetLabel,
+                x: labels,
+                y: values,
+                type: type,
+                mode: mode,
+                marker: {
+                    size: 10,
                 }
             }
-        },
+        ];
+
+        chartLayout = {
+            autosize: true,
+            title: title,
+            showlegend: true,
+            legend: {
+                x: 1,
+                xanchor: 'right',
+                yanchor: 'top',
+                y: 1,
+            },
+            font: {
+                size: 30,
+            },
+            yaxis: {
+                fixedrange: true,
+            }
+        }
+
+        chartConfig = {
+            scrollZoom: true,
+            responsive: true,
+        }
     };
 
-    var myChart = new Chart("myChart", config);
+    function drawChart() {
+        $("#errMsg").remove();
+        if (!allData || chartData[0].x.length == 0) {
+            errMsg();
+        }
+
+        Plotly.newPlot(myChart, chartData, chartLayout, chartConfig);
+    } 
+
+    function getData(station, airType) {
+        const xhr = new XMLHttpRequest();
+        xhr.open("GET", dataEndpoint, true);
+        xhr.send();
+        xhr.onerror = () => {
+            alert("Опа, някой омаза работата.");
+            updateChart();
+        }
+
+        xhr.onload = () => {
+            allData = JSON.parse(xhr.responseText);
+            updateChart();
+            drawChart();
+        };
+    }
+
+    getData(station.value, airType.value)
 
     $("#chartType").change(function() {
-        myChart.destroy();
-        config.type = event.target.value;
-        myChart = new Chart("myChart", config);
+        updateChart();
+        drawChart();
+    });
+
+    $("#station").change(function() {
+        getData(station.value, airType.value);
+    });
+
+    $("#airType").change(function() {
+        updateChart();
+        drawChart();
     });
 };
