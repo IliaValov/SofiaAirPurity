fc96eb9fe295ba70fe6b2d105ce1c478541a03bc
Pavel Sarlov <sarlovpavel@gmail.com>
2021-12-03 01:20:10 +0200

    finalize the app

--- src/app/main/routes.py
+++ src/app/main/routes.py
@@ -1,13 +1,20 @@
 from flask import current_app as app, g, session, render_template, redirect, url_for, Blueprint, json, request
 import os, random
-from datetime import timedelta, date
+from datetime import datetime
 from app.parser.parser import *
 from app.storage.dataStorage import *
 
 main = Blueprint("main", __name__)
 
-@main.route("/", methods=["POST", "GET"])
+@main.route("/", methods=["GET", "POST"])
 def index():
+    if request.method=="POST":
+        return redirect(url_for(request.form.get("type")))
+
+    return render_template("index.html")
+
+@main.route("/range", methods=["POST", "GET"])
+def charts_range():
     if request.method=="POST":
         pars = Parser(DataStorage().getAirData())
         rqst = json.loads(request.get_data())
@@ -20,4 +27,29 @@ def index():
         )
         return response 
 
-    return render_template("index.html")
+    return render_template("charts-range.html")
+
+@main.route("/day", methods=["POST", "GET"])
+def charts_day():
+    if request.method=="POST":
+        pars = Parser(DataStorage().getAirData())
+        rqst = json.loads(request.get_data())
+        data = pars.getDataByStationAndPolluter(int(rqst.get("station")), int(rqst.get("airType")))
+        filtered = {} 
+        for i,v in enumerate(data.get('timeSet')):
+            x = datetime.strptime(v, "%Y-%m-%d %H:%M:%S")
+            d = str(datetime.date(x))
+            t = datetime.time(x).strftime('%H:%M:%S')
+            if d not in filtered:
+                filtered[d] = { 'time': [], 'level': [] }
+            filtered[d]['time'].append(t)
+            filtered[d]['level'].append(data.get('level')[i])
+
+        response = app.response_class(
+            response=json.dumps(filtered),
+            status=200,
+            mimetype="application/json"
+        )
+        return response 
+
+    return render_template("charts-day.html")
--- src/app/static/js/chart-range.js
+++ src/app/static/js/chart-range.js
@@ -1,11 +1,7 @@
 window.onload = function() {
-    const dataEndpoint = "/";
-    var chartType = $("#chartType").get(0);
-    var station = $("#station").get(0);
-    var airType = $("#airType").get(0);
+    const dataEndpoint = "/range";
     var allData = null;
 
-    var myChart = $("#myChart").get(0);
     var chartLoaded = false;
     var chartData = null;
     var chartLayout = null;
@@ -13,70 +9,77 @@ window.onload = function() {
 
     var dateLower = 0;
     var dateUpper = 0;
-    var dateSlider = $("#dateSlider").get(0);
-    dateSlider.style.pointerEvents = "none";
+
+    $('#dateSlider').css('pointer-events', "none");
 
     var slider = wRunner({
-        roots: dateSlider,
+        roots: $('#dateSlider').get(0),
         type: 'range',
         rangeValue: {
             minValue: 0,
             maxValue: 1,
         },
         step: 1,
-        direction: 'vertical',
+        direction: 'horizontal',
     });
-    slider.onValueUpdate(function() { 
+    slider.onValueUpdate(function() {
         if (chartLoaded) {
             values = slider.getValue();
-            dateLower=values["minValue"];
-            dateUpper=values["maxValue"];
-            $(".wrunner__valueNote")[0].innerHTML=allData["timeSet"][dateLower];
-            $(".wrunner__valueNote")[1].innerHTML=allData["timeSet"][dateUpper];
-            updateChart();
+            dateLower = values["minValue"];
+            dateUpper = values["maxValue"];
+            if (allData) {
+                $(".wrunner__valueNote")[0].innerHTML = allData["timeSet"][dateLower];
+                $(".wrunner__valueNote")[1].innerHTML = allData["timeSet"][dateUpper];
+                updateChart();
+            }
         }
     });
     slider.onLimitsUpdate(function() {
         limits = slider.getLimits();
-        slider.setRangeValue({ 
-            'minValue': limits["minLimit"], 
-            'maxValue': limits["maxLimit"], 
+        slider.setRangeValue({
+            'minValue': limits["minLimit"],
+            'maxValue': limits["maxLimit"],
         });
-        $(".wrunner__valueNote")[0].innerHTML=allData["timeSet"][limits["minLimit"]];
-        $(".wrunner__valueNote")[1].innerHTML=allData["timeSet"][limits["maxLimit"]];
+        if (allData) {
+            $(".wrunner__valueNote")[0].innerHTML = allData["timeSet"][limits["minLimit"]];
+            $(".wrunner__valueNote")[1].innerHTML = allData["timeSet"][limits["maxLimit"]];
+        }
     });
 
     function errMsg() {
         var errMsg = $("<div id='errMsg'>ЛИПСВАТ ДАННИ ИЛИ СА НЕВАЛИДНИ &#x2639;</div>");
-        $(".chart-container").get(0).appendChild(errMsg.get(0));
+        errMsg.appendTo(".chart-container");
     }
 
     function updateLimits(min, max) {
         limits = slider.getLimits();
 
-        if (limits['minLimit']!=min || limits['maxLimit']!=max) {
-            slider.setLimits({ 
-                'minLimit': min, 
-                'maxLimit': max 
+        if (limits['minLimit'] != min || limits['maxLimit'] != max) {
+            slider.setLimits({
+                'minLimit': min,
+                'maxLimit': max
             });
         }
     }
 
     function updateChart() {
+        $("#errMsg").remove();
+        $('#chartType').attr('disabled', false);
+        $('#dateSlider').css('pointer-events', "auto");
+
         chartLoaded = false;
-        var type = chartType.value;
+        var type = $('#chartType').find(':selected').attr('value');
         var mode = "lines+markers";
         const labels = allData ? allData["timeSet"].slice(dateLower, dateUpper) : [];
         const values = allData ? allData["level"].slice(dateLower, dateUpper) : [];
-        const title = station.options[station.selectedIndex].text;
-        const datasetLabel = airType.options[airType.selectedIndex].text;
+        const title = $('#station').find(':selected').text();
+        const datasetLabel = $('#airType').find(':selected').text();
 
-        updateLimits(0, allData ? allData["timeSet"].length-1 : 1);
+        updateLimits(0, allData ? allData["timeSet"].length - 1 : 1);
 
         if (type == "scattergl") {
             mode = "markers";
-        }
-        else if (type == "line") {
+        } else if (type == "line") {
             type = 'scattergl';
         }
 
@@ -113,6 +116,13 @@ window.onload = function() {
                 tickfont: {
                     family: 'Old Standard TT, serif',
                     size: 20,
+                },
+                title: {
+                    text: $('#airType').find(':selected').attr('unit'),
+                    font: {
+                        family: 'Old Standard TT, serif, bold',
+                        size: 24,
+                    }
                 }
             },
             xaxis: {
@@ -123,6 +133,13 @@ window.onload = function() {
                 tickfont: {
                     family: 'Old Standard TT, serif',
                     size: 20,
+                },
+                title: {
+                    text: 'Период',
+                    font: {
+                        family: 'Old Standard TT, serif',
+                        size: 24,
+                    }
                 }
             }
         }
@@ -132,29 +149,30 @@ window.onload = function() {
             responsive: true,
         }
 
-        $("#errMsg").remove();
-        chartType.disabled = false;
-        dateSlider.style.pointerEvents = "auto";
-
         if (!allData || chartData[0].x.length == 0) {
             errMsg();
             chartConfig.staticPlot = true;
-            chartType.disabled = true;
-            dateSlider.style.pointerEvents = "none";
+            $('#chartType').attr('disabled', true);
+            $('#dateSlider').css({
+                'pointer-events': 'none',
+                'visibility': 'hidden'
+            });
         }
 
         Plotly.newPlot(myChart, chartData, chartLayout, chartConfig);
-        chartLoaded=true;
+        chartLoaded = true;
     };
 
-    function getData(station, airType) {
+    function getData() {
         const xhr = new XMLHttpRequest();
         xhr.open("POST", dataEndpoint, true);
         xhr.setRequestHeader('Content-Type', 'application-json');
         xhr.send(JSON.stringify({
-            'station': station,
-            'airType': airType
+            'station': $('#station').find(':selected').attr('value'),
+            'airType': $('#airType').find(':selected').attr('value'),
         }));
+        $("#loading").css("display", "flex");
+
         xhr.onerror = () => {
             alert("Опа, някой омаза работата.");
             updateChart();
@@ -165,20 +183,21 @@ window.onload = function() {
             dateLower = 0;
             dateUpper = allData ? allData["timeSet"].length : 1;
             updateChart();
+            $("#loading").css("display", "none");
         };
     }
 
-    getData(station.value, airType.value)
+    getData();
 
     $("#chartType").change(function() {
         updateChart();
     });
 
     $("#station").change(function() {
-        getData(station.value, airType.value);
+        getData();
     });
 
     $("#airType").change(function() {
-        getData(station.value, airType.value);
+        getData();
     });
 };
